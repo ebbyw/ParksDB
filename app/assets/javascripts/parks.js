e0bc20bb5c485071ddb290cd6d8a2d04d1bb0ec5
Ebby Wahman <ebbywahman@gmail.com>
2017-04-09 16:11:08 -0700

    add google map

--- app/assets/javascripts/parks.js
+++ app/assets/javascripts/parks.js
@@ -1,3 +1,19 @@
 # Place all the behaviors and hooks related to the matching controller here.
 # All this logic will automatically be available in application.js.
 # You can use CoffeeScript in this file: http://coffeescript.org/
+
+function initMap() {
+	var uluru = {lat: -25.363, lng: 131.044};
+	var map = new google.maps.Map(document.getElementById('map'), {
+	  zoom: 4,
+	  center: uluru
+	});
+	var marker = new google.maps.Marker({
+		position: uluru,
+		map: map
+	});
+}
+
+async defer
+    src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBBSaQmP0Ff65kmrv1iYc6O7Pq5SY5eclc&callback=initMap">
+
